f893f7398b642f9f90e7c2de90443843df82380c
Ernest Guevarra <ernest@guevarra.io>
2026-06-26 11:37:59 +0100

    retrieve pdf files and convert each page to jpeg

--- _targets.R
+++ _targets.R
@@ -1,46 +1,69 @@
-################################################################################
-#
-# General Targets Workflow
-#
-################################################################################
+# Seychelles Mortality Data Extraction -----------------------------------------
 
-## Load libraries and custom functions -----------------------------------------
+
+## Load libraries and custom functions ----
 suppressPackageStartupMessages(source("packages.R"))
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
-## Create targets and list targets objects -------------------------------------
 
-### Data targets
+## Data targets
 data_targets <- tar_plan(
-  
+  tar_target(
+    name = pdf_file_paths,
+    command = list.files(
+      path = "data-raw/pdf",
+      full.names = TRUE, recursive = TRUE 
+    )
+  ),
+  tar_target(
+    name = pdf_file_key,
+    command = create_pdf_file_key(pdf_file_paths)
+  ),
+  tar_target(
+    name = pdf_page_numbers,
+    command = lapply(
+      X = pdf_file_key$page, FUN = function(x) eval(parse(text = x))
+    )
+  ),
+  tar_target(
+    name = jpeg_image_paths,
+    command = convert_pdf_to_images(
+      pdf = pdf_file_paths, 
+      format = "jpeg", 
+      page = pdf_page_numbers, 
+      destdir = "data-raw/jpg", 
+      dpi = 150
+    ),
+    pattern = map(pdf_file_paths, pdf_page_numbers)
+  )
 )
 
 
-### Processing targets
+## Processing targets
 processing_targets <- tar_plan(
   
 )
 
 
-### Analysis targets
+## Analysis targets
 analysis_targets <- tar_plan(
   
 )
 
 
-### Output targets
+## Output targets
 output_targets <- tar_plan(
   
 )
 
 
-### Reporting targets
+## Reporting targets
 report_targets <- tar_plan(
   
 )
 
 
-### Deploy targets
+## Deploy targets
 deploy_targets <- tar_plan(
   
 )
