3b8530c61d118bcd31d187633b52e134f760e4eb
Ernest Guevarra <ernest@guevarra.io>
2026-06-26 15:44:10 +0100

    create testing outputs for validation

---
+++ R/output_test_results.R
@@ -0,0 +1,30 @@
+#'
+#' Output extraction test results to an Excel file
+#' 
+
+output_test_results <- function(extraction_test_results,
+                                output_file,
+                                overwrite = FALSE) {
+  # Create a new workbook
+  wb <- openxlsx::createWorkbook()
+  
+  sheetnames <- names(extraction_test_results)
+
+  Map(
+    f = openxlsx::addWorksheet,
+    sheetName = sheetnames,
+    MoreArgs = list(wb = wb)
+  )
+
+  Map(
+    f = openxlsx::writeData,
+    sheet = sheetnames,
+    x = extraction_test_results,
+    MoreArgs = list(wb = wb)
+  )
+  
+  # Save the workbook to the specified output file
+  openxlsx::saveWorkbook(wb, file = output_file, overwrite = overwrite)
+ 
+  output_file
+}
